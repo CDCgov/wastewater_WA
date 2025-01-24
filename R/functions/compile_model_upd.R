f00664875e2ab86a95e7976dc6b7f5fd11023a50
arranhamlet <arran.hamlet14@imperial.ac.uk>
2025-01-24 09:29:13 -0800

    Additional update to model compiling

--- R/functions/compile_model_upd.R
+++ R/functions/compile_model_upd.R
@@ -15,7 +15,8 @@ compile_model_upd <- function(custom_location = F, update_files = F){
       reconstructed_path <- if(grepl(" ", stan_location)) deconstructed_path[1:which(grepl(" ", deconstructed_path))-1] else deconstructed_path
       
       #Put back together above the portion with a 
-      new_folder <- paste0(if(custom_location == T) custom_location else reconstructed_path, "/wastewater_stan_files/")
+      new_folder <- paste0(if(custom_location != F) custom_location else reconstructed_path, "/wastewater_stan_files/", collapse = "")
+      new_folder <- gsub("//", "/", new_folder)
       
       #Delete directory if it exists
       unlink(new_folder, recursive = TRUE)
